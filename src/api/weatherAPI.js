096e52663dfe7def3960b215a49392d248d09ffe
xtang999 <xiaoli05tang@gmail.com>
2023-11-14 16:13:42 +1100

    enable cors in api call

--- src/api/weatherAPI.js
+++ src/api/weatherAPI.js
@@ -1,7 +1,8 @@
 // weatherAPI.js
 
 export async function getWeather(city) {
-    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=8a3ba5487b77443dbaa44807231211&q=${city}&days=7`);
+    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=8a3ba5487b77443dbaa44807231211&q=${city}&days=7`, 
+    {mode: 'cors'});
     const data = await response.json();
     console.log(data);
     return data;
