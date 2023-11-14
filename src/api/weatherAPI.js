5b6254817007531f5c9fd7d1831cc66c2795e7f4
xtang999 <xiaoli05tang@gmail.com>
2023-11-14 16:03:12 +1100

    change api request to https

--- src/api/weatherAPI.js
+++ src/api/weatherAPI.js
@@ -1,7 +1,7 @@
 // weatherAPI.js
 
 export async function getWeather(city) {
-    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=8a3ba5487b77443dbaa44807231211&q=${city}&days=7`);
+    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=8a3ba5487b77443dbaa44807231211&q=${city}&days=7`);
     const data = await response.json();
     console.log(data);
     return data;
